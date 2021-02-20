5c811a662bfbc5d67a779d876a154625e1575b6f
Salih Bora ÖZTÜRK <salihb26@gmail.com>
2021-02-20 14:44:52 +0300

    FluentValidation yapısı

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,10 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -22,37 +25,30 @@ namespace Business.Concrete
 
         public IResult Add(Car car)
         {
-            if(car.DailyPrice>0 && car.CarName.Length > 2)
-            {
-                _iCarDal.Add(car);
-                return new SuccessResult(Messages.CarAdded);
-            }
-            else
-            {
-              
-           return new ErrorResult(Messages.CarCouldNotAdded);
-            }
-           
+            ValidationTool.Validate(new CarValidator(), car);
+            _iCarDal.Add(car);
+            return new SuccessResult(Messages.CarAdded);
+
         }
 
         public IDataResult<List<Car>> GetAll()
         {
-            return new SuccessDataResult<List<Car>>(_iCarDal.GetAll(),Messages.CarsDisplay);
+            return new SuccessDataResult<List<Car>>(_iCarDal.GetAll(), Messages.CarsDisplay);
         }
 
         public IDataResult<List<CarDetailDto>> GetCarDetails()
         {
-            return new SuccessDataResult<List<CarDetailDto>>(_iCarDal.GetCarDetails(),Messages.CarsDisplay);
+            return new SuccessDataResult<List<CarDetailDto>>(_iCarDal.GetCarDetails(), Messages.CarsDisplay);
         }
 
         public IDataResult<List<Car>> GetCarsByBrandId(int id)
         {
-            return new SuccessDataResult<List<Car>>(_iCarDal.GetAll(p=>p.BrandId==id),Messages.CarsDisplay);
+            return new SuccessDataResult<List<Car>>(_iCarDal.GetAll(p => p.BrandId == id), Messages.CarsDisplay);
         }
 
         public IDataResult<List<Car>> GetCarsByColorId(int id)
         {
-            return new SuccessDataResult<List<Car>>(_iCarDal.GetAll(p => p.ColorId == id),Messages.CarsDisplay);
+            return new SuccessDataResult<List<Car>>(_iCarDal.GetAll(p => p.ColorId == id), Messages.CarsDisplay);
         }
     }
 }
---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c=>c.CarName).MinimumLength(2);
+            RuleFor(c => c.CarName).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.ModelYear).GreaterThan(1950);
+            RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(1).When(c=>c.BrandId==1);
+            RuleFor(c => c.CarName).Must(StartsWithA).WithMessage("Ürünler A harfi ile başlamalı.");
+
+        }
+
+        private bool StartsWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
