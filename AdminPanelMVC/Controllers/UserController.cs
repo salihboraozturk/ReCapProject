2508139b458af33f2c79a7a330e34f7abf9bf66a
Salih Bora ÖZTÜRK <salihb26@gmail.com>
2021-07-28 18:02:36 +0300

    MVC Admin Panel User Management Listing Operation

---
+++ AdminPanelMVC/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminPanelMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        private IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;   
+        }
+        public IActionResult Index()
+        {
+            var users = _userService.GetAll().Data;
+            return View(users);
+        }
+    }
+}
