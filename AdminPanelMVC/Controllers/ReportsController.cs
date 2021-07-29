3b7a51dacc2cc97a1ffa474b6678d1c409db81d1
Salih Bora ÖZTÜRK <salihb26@gmail.com>
2021-07-29 18:07:45 +0300

    MVC Admin Panel Brand Reports

--- AdminPanelMVC/Controllers/ReportsController.cs
+++ AdminPanelMVC/Controllers/ReportsController.cs
@@ -10,14 +10,21 @@ namespace AdminPanelMVC.Controllers
     public class ReportsController : Controller
     {
         private ICarService _carService;
-        public ReportsController(ICarService carService)
+        private IBrandService _brandService;
+        public ReportsController(ICarService carService, IBrandService brandService)
         {
             _carService = carService;
+            _brandService = brandService;
         }
         public IActionResult Index()
         {
             var carsDetails = _carService.GetAllCarDetails().Data;
             return View(carsDetails);
+        }
+        public IActionResult Brand()
+        {
+            var brand = _brandService.GetAll().Data;
+            return View(brand);
         }
     }
 }
