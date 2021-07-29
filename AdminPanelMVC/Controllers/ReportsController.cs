1378332ddd53c864817fbd95a876888bd2a0cd85
Salih Bora ÖZTÜRK <salihb26@gmail.com>
2021-07-29 17:22:19 +0300

    MVC Admin Panel Cars Reports View

---
+++ AdminPanelMVC/Controllers/ReportsController.cs
@@ -0,0 +1,23 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminPanelMVC.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private ICarService _carService;
+        public ReportsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+        public IActionResult Index()
+        {
+            var carsDetails = _carService.GetAllCarDetails().Data;
+            return View(carsDetails);
+        }
+    }
+}
